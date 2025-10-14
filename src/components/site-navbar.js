24bef7a9056c29ab599dcdbe8efcdf09d6242f27
Alex Lu <alu59@my.bcit.ca>
2025-10-14 12:50:41 -0700

    Finish login and sign up capabilities

--- src/components/site-navbar.js
+++ src/components/site-navbar.js
@@ -1,17 +1,26 @@
+// Import specific functions from the Firebase Auth SDK
+import {
+    onAuthStateChanged,
+} from "firebase/auth";
+
+import { auth } from '/src/firebaseConfig.js';
+import { logoutUser } from '/src/authentication.js';
+
 class SiteNavbar extends HTMLElement {
     constructor() {
         super();
         this.renderNavbar();
+        this.renderAuthControls();
     }
 
     renderNavbar() {
         this.innerHTML = `
             <!-- Navbar: single source of truth -->
-            <nav class="navbar navbar-expand-lg navbar-light bg-info">
+            <nav class="navbar navbar-expand-lg navbar-light bg-secondary">
                 <div class="container-fluid">
-                    <a class="navbar-brand" href="/">
-                        <img src="/images/image.jpg" height="36">
-                        ElmoHikes
+                    <a class="navbar-brand text-white" href="/">
+                        <img src="/images/image.jpg" height="60">
+                        AlexHikes
                     </a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
                         aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -20,7 +29,7 @@ class SiteNavbar extends HTMLElement {
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="navbar-nav me-auto">
                             <li class="nav-item">
-                                <a class="nav-link" href="/">Home</a>
+                                <a class="nav-link text-white" href="/">Home</a>
                             </li>
                         </ul>
                         <div class="d-flex align-items-center gap-2 ms-lg-2" id="rightControls">
@@ -37,6 +46,26 @@ class SiteNavbar extends HTMLElement {
                 </div>
             </nav>
         `;
+    }
+
+    renderAuthControls() {
+        const authControls = this.querySelector('#authControls');
+
+        // Initialize with invisible placeholder to maintain layout space
+        authControls.innerHTML = `<div class="btn btn-outline-light" style="visibility: hidden; min-width: 80px;">Log out</div>`;
+
+        onAuthStateChanged(auth, (user) => {
+            let updatedAuthControl;
+            if (user) {
+                updatedAuthControl = `<button class="btn btn-outline-light" id="signOutBtn" type="button" style="min-width: 80px;">Log out</button>`;
+                authControls.innerHTML = updatedAuthControl;
+                const signOutBtn = authControls.querySelector('#signOutBtn');
+                signOutBtn?.addEventListener('click', logoutUser);
+            } else {
+                updatedAuthControl = `<a class="btn btn-outline-light" id="loginBtn" href="/login.html" style="min-width: 80px;">Log in</a>`;
+                authControls.innerHTML = updatedAuthControl;
+            }
+        });
     }
 }
 
